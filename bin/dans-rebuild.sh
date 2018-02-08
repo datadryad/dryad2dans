cec740393aa0525f7064d1d703824c9526fee047
Ryan Scherle <ryan@scherle.org>
2018-02-08 16:59:17 +0000

    Update for new default machine setup

--- bin/dans-rebuild.sh
+++ bin/dans-rebuild.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Script to rebuild the Dryad2Dans package and install an updated version. This *only*
-# works when Dryad2Dans has already been installed on the machine, using the standard 
-# Dryad directory layout.
+# Rebuilds and deploys the local installation of the Dryad2Dans module, for
+# deploying newly updated code. It is assumed that the module has previously
+# been installed, and all prerequisites are in place.
 
 echo Rebuilding the DANS installation...
 set -e
 
-cd /home/ec2-user/dryad2dans
+cd /home/ubuntu/dans-bagit
+mvn install
+cd /home/ubuntu/dryad2dans
 mvn clean package
 mvn dependency:copy-dependencies -DexcludeArtifactIds=junit,api,dspace-api,xom,xml-apis,xercesImpl,xalan,commons-codec,commons-httpclient,commons-logging,log4j,geronimo-stax-api_1.0_spec,wstx-asl,jaxen,lucene-core,icu4j,stax-api,xmlParserAPIs,rome,jdom,commons-cli,commons-lang,joda-time,servlet-api,commons-io
 sudo cp target/dependency/* /opt/dryad/lib/
 sudo cp target/dans-1.0-SNAPSHOT.jar /opt/dryad/lib
-sudo /opt/dryad/bin/dspace registry-loader -dc /home/ec2-user/dryad2dans/config/registries/dans-metadata.xml
 
 echo
 echo     ====  REBUILD SUCCEEDED ====
