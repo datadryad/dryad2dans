a1a391b1ec50ee7255a5b00e34b5ae2a06bb954d
Ryan Scherle <ryan@scherle.org>
2018-03-27 16:42:04 +0000

    Improve transfer script, add generator for transfer list

---
+++ bin/dans-make-item-list.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script to generate a list of items to be transferred to DANS
+# Assumes the PGHOST and PGPASSWORD environment variables are set.
+
+echo Creating list of items to transfer to DANS...
+
+NUM_ITEMS=1000;
+DANS_METADATA_FIELD=159
+
+psql -qt -U dryad_app -d dryad_repo -c "select item_id from item where in_archive = true and owning_collection=2 and item_id NOT IN (Select distinct item_id from metadatavalue where metadata_field_id=$DANS_METADATA_FIELD) order by item_id asc limit $NUM_ITEMS;" -o items.txt
+
+echo List of items is in items.txt
+
