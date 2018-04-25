5ba2b1758e6c5a634d680b87fae01f6d46d88322
Ryan Scherle <ryan@scherle.org>
2018-04-25 16:43:02 +0000

    Change the default location for staging files

--- bin/dans-transfer-many.sh
+++ bin/dans-transfer-many.sh
@@ -3,7 +3,7 @@ echo Transferring item to DANS...
 set -e
 
 while read item; do
-    sudo /opt/dryad/bin/dspace dsrun org.datadryad.dans.DANSTransfer -i $item -d -t /opt/dryad-data/tmp/
+    sudo /opt/dryad/bin/dspace dsrun org.datadryad.dans.DANSTransfer -i $item -d -t /transfer-complete/dans-transfer
 done <items.txt
 
 echo
--- bin/dans-transfer.sh
+++ bin/dans-transfer.sh
@@ -7,7 +7,7 @@
 echo Transferring item to DANS...
 set -e
 
-sudo /opt/dryad/bin/dspace dsrun org.datadryad.dans.DANSTransfer -i $1 -d -t /opt/dryad-data/tmp/
+sudo /opt/dryad/bin/dspace dsrun org.datadryad.dans.DANSTransfer -i $1 -d -t /transfer-complete/dans-transfer
 
 echo
 
