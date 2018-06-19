a286d9c03f136082cb898eedbda22afa20eb7fc6
Ryan Scherle <ryan@scherle.org>
2018-06-19 15:21:30 +0000

    Improve dans-all.sh
    Multiple fixes to make it easier to run this script from cron:
    - Allow user to specify the location for temporary and output files.
    - Use a lock file to prevent the script from running multiple simultaneous copies.
    - Change output filename to include hour and day of week, so some history will remain in the directory.

--- bin/dans-all.sh
+++ bin/dans-all.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-# Script to transfer all Dryad content to DANS
+# Script to transfer all new Dryad content to DANS
+# 
+# Usage: dans-all.sh <temp-directory>
 
-echo Transferring item to DANS...
+if [ $# -gt 0 ]; then
+    DANS_TEMP=$1
+else 
+    DANS_TEMP="/opt/dryad-data/tmp"
+fi
 
-sudo /opt/dryad/bin/dspace dsrun org.datadryad.dans.DANSTransfer --all -d -t /opt/dryad-data/tmp/ > /opt/dryad-data/tmp/dansAll.out &
+LOCKFILE="/var/lock/.dans-export.exclusivelock"
+day=`date "+%a"`
+hour=`date "+%H"`
 
-echo output being sent to /opt/dryad-data/tmp/dansAll.out
+# ensure there is only one copy of this script running
+lock() {
+    exec 200>$LOCKFILE
+
+    flock -n 200 \
+        && return 0 \
+        || return 1
+}
+lock || { exit 1; }
+
+# do the transfer
+echo "Transferring item to DANS using temp directory $DANS_TEMP"
+
+sudo /opt/dryad/bin/dspace dsrun org.datadryad.dans.DANSTransfer --all -d -t $DANS_TEMP/ > $DANS_TEMP/dansAll-${day}_${hour}.out & 
+
+
 
