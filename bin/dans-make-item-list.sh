481fec794b15bacecd9399e93374f83ef67b040b
Ryan Scherle <ryan@scherle.org>
2018-07-01 14:11:52 +0000

    Update field for checking whether items are archived in DANS
    This script was previously checking for items that have been transferred, but some transferred
    items have failed to process on the DANS end. Updating the script to check for items that are
    archived at DANS.

--- bin/dans-make-item-list.sh
+++ bin/dans-make-item-list.sh
@@ -6,7 +6,7 @@
 echo Creating list of items to transfer to DANS...
 
 NUM_ITEMS=5000;
-DANS_METADATA_FIELD=159
+DANS_METADATA_FIELD=162
 
 psql -qt -U dryad_app -d dryad_repo -c "select item_id from item where in_archive = true and owning_collection=2 and item_id NOT IN (Select distinct item_id from metadatavalue where metadata_field_id=$DANS_METADATA_FIELD) order by item_id asc limit $NUM_ITEMS;" -o items.txt
 
