ebdf33d1f68586976f048841e800fe4a297f70df
Ryan Scherle <ryan@scherle.org>
2018-07-13 16:43:59 +0000

    Remove old metadata before the transfer creates new metadata
    For items that previously had a failed transfer, we do not want the old
    (useless) edit IRI in the metadata. Clear the DANS fields, while retaining
    the provenance history, so newly transferred items can be tracked correctly.

--- bin/dans-transfer-many.sh
+++ bin/dans-transfer-many.sh
@@ -3,6 +3,11 @@ echo Transferring item to DANS...
 set -e
 
 while read item; do
+    # Clear metadata regarding previous DANS transfers, so there are no confusing edit IRIs
+    # Note that provenance fields will not be cleared, so the transfer history is still human-readable
+    sudo /opt/dryad/bin/dspace dsrun org.datadryad.dans.TransferControl -i $item -c
+
+    # Perform the transfer
     sudo /opt/dryad/bin/dspace dsrun org.datadryad.dans.DANSTransfer -i $item -d -t /transfer-complete/dans-transfer
 done <items.txt
 
