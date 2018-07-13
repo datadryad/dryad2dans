236687c88dd55e30d942386d3e5c64ab3614af75
Ryan Scherle <ryan@scherle.org>
2018-07-13 16:54:39 +0000

    Clear metadata for individual transfers, too

--- bin/dans-transfer.sh
+++ bin/dans-transfer.sh
@@ -7,6 +7,11 @@
 echo Transferring item to DANS...
 set -e
 
+# Clear metadata regarding previous DANS transfers, so there are no confusing edit IRIs
+# Note that provenance fields will not be cleared, so the transfer history is still human-readable
+sudo /opt/dryad/bin/dspace dsrun org.datadryad.dans.TransferControl -i $1 -c
+
+# Perform the transfer
 sudo /opt/dryad/bin/dspace dsrun org.datadryad.dans.DANSTransfer -i $1 -d -t /transfer-complete/dans-transfer
 
 echo
