cb6ebbdc4edaddb5ae37127a28cf6467e374e794
Ryan Scherle <ryan@scherle.org>
2018-08-10 14:04:26 +0000

    script update

--- bin/dans-monitor.sh
+++ bin/dans-monitor.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-# Script to transfer a single item from Dryad to DANS
+# Script to mointor a single item from Dryad to DANS transfer
 #
-# Usage dans-transfer.sh item-id
+# Usage dans-transfer.sh [item-id]
+# If no item-id is transferred, will search the database for transferred
+# items and monitor each of them.
 
 echo Monitoring previously transferred items...
 set -e
 
-sudo /opt/dryad/bin/dspace dsrun org.datadryad.dans.DANSTransfer -m -a
+if [ $# -gt 0 ]; then
+    # monitor one item by ID
+    echo "Monitoring $1"
+    sudo /opt/dryad/bin/dspace dsrun org.datadryad.dans.DANSTransfer -m -i $1
+else 
+    echo "Monitoring all items in the database"
+    sudo /opt/dryad/bin/dspace dsrun org.datadryad.dans.DANSTransfer -m -a
+fi
 
-echo
 
