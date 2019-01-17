f50b30885c604200c3110c0ea1430bd61b83d834
Ryan Scherle <ryan@scherle.org>
2019-01-17 18:49:35 +0000

    use default file archived-items.txt

--- bin/dans-set-archived-many.sh
+++ bin/dans-set-archived-many.sh
@@ -1,7 +1,7 @@
 # Sets a list of items to have metadata indicating they are archived in DANS
 
 # It is assumed that the list of items is stored in a 
-# file in the current directory called items.txt,
+# file in the current directory called archived-items.txt,
 # with one item ID per line.
 
 echo "Setting items as archived in DANS..."
@@ -11,7 +11,7 @@ set -e
 while read item; do
     # Perform the transfer
     sudo /opt/dryad/bin/dspace dsrun org.datadryad.dans.DANSTransfer -i $item -s
-done <items.txt
+done <archived-items.txt
 
 echo
 
